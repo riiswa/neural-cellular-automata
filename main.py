89b37e5a8ec32bc58cce59b331945d202461e079
Waris Radji <wradji64@gmail.com>
2023-02-01 11:34:49 +0100

    Back to mse loss

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ import random
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 import os
-from pytorch_msssim import SSIM
 
 
 def load_image_from_url(url, size=64):
@@ -103,7 +102,7 @@ class NeuralCellularAutomata(nn.Module):
         self.dense2 = nn.Linear(128, 16)
         self.dense2.weight.data.zero_()
 
-        self.loss_fn = SSIM(data_range=1.0, channel=3).to(self.device)
+        self.loss_fn = nn.MSELoss().to(self.device)
         self.optimizer = torch.optim.Adam(self.parameters(), lr=2e-3)
         self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=1000, gamma=0.5)
         self.to(self.device)
@@ -146,7 +145,7 @@ class NeuralCellularAutomata(nn.Module):
             idxs, batch = sample()
             with torch.no_grad():
                 max_idx = max(range(batch_size),
-                              key=lambda x: 1-self.loss_fn(
+                              key=lambda x: self.loss_fn(
                                   batch[x][:, :, :4].transpose(0, 2).unsqueeze(0).clamp(0, 1).to(self.device),
                                   target.transpose(0, 2).unsqueeze(0).clamp(0, 1).to(self.device))
                               )
@@ -156,7 +155,7 @@ class NeuralCellularAutomata(nn.Module):
             state_grids = torch.stack(batch)
             outputs = self.update(state_grids.to(self.device))
             del state_grids
-            loss = 1-self.loss_fn(outputs[:, :, :, :4].clamp(0, 1).transpose(1, 3), targets.transpose(1, 3))
+            loss = self.loss_fn(outputs[:, :, :, :4].clamp(0, 1).transpose(1, 3), targets.transpose(1, 3))
             loss.backward()
             self.writer.add_scalar("Loss", loss.item(), i)
             self.optimizer.step()
